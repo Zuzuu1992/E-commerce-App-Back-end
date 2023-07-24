485ec7d272745b7ba230afc177598b70db80574c
Ana Izoria <ana.izoria999@gmail.com>
2023-07-24 18:44:59 +0400

    feat:setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import express from "express";
+import { createTable } from "./config/sql.js";
 
 const app = express();
 
-app.get("/", (req, res)=>{
-  return res.status(200).json{message: "App works!"}
-})
+async function init() {
+  try {
+    await createTable();
+    startServer();
+  } catch (error) {
+    console.log(error);
+  }
+
+  function startServer() {
+    app.get("/", (req, res) => {
+      return res.status(200).json({ message: "App works!" });
+    });
+    app.listen(3000);
+  }
+}
+
+init();
---
+++ src/config/sql.js
@@ -0,0 +1,32 @@
+import pgk from "pg";
+
+const { Pool } = pgk;
+
+const pool = new Pool({
+  host: "dpg-civ89n5iuiedpv15vjrg-a",
+  port: "5432",
+  database: "e_commerce_app_rjct",
+  user: "e_commerce_app_rjct_user",
+  password: "QSNCu65ib0fTgrtuCFBFFXXwzhWO8fFL",
+});
+
+export const createTable = async () => {
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS products(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        quantity INT NOT NULL
+      )
+    `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS orders(
+        id SERIAL PRIMARY KEY,
+        product_id INT NOT NULL,
+        quantity INT NOT NULL,
+        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (product_id) REFERENCES products(id)
+      )
+    `);
+};
